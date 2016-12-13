2184c8061a77b2d70c034c08b6536cb15f3f0c53
allwillburn <livefortoday470@gmail.com>
2016-12-12 19:22:45 -0500

    Update Camille.lua

--- Camille.lua
+++ Camille.lua
@@ -156,7 +156,7 @@ OnTick(function (myHero)
 	    end
 	    
 	    
-            if CamilleMenu.Combo.R:Value() and Ready(_R) and ValidTarget(target, 700) and (EnemiesAround(myHeroPos(), 700) >= CalmilleMenu.Combo.RX:Value()) then
+            if CamilleMenu.Combo.R:Value() and Ready(_R) and ValidTarget(target, 700) and (EnemiesAround(myHeroPos(), 700) >= CamilleMenu.Combo.RX:Value()) then
 			CastSkillShot(_R, target) 
             end
 
